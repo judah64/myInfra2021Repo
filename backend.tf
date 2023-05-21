5aad47e5cc793ffaf5dc0dad7e6fe7818e778710
judah64 <48566693+judah64@users.noreply.github.com>
2023-05-20 20:57:13 -0400

    Update backend.tf
    update name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "femi-dev-tf-state-bucket"
+    bucket = "my-dev-tf-state-bucket-femi"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamo-db-table"
